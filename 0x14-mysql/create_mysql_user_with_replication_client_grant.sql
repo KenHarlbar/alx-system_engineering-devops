ed42441ab2956506d27b388d78ba9b251cc92e88
baaioy <balotech514@gmail.com>
2022-10-12 15:56:40 +0100

    Create a new mysql user with password and replication client privilege

---
+++ 0x14-mysql/create_mysql_user_with_replication_client_grant.sql
@@ -0,0 +1,5 @@
+-- Create user and password with replication client grant
+
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
