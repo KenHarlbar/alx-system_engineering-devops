fd1a3bdb87fa133c23395b37672795ec471a2142
baaioy <balotech514@gmail.com>
2022-10-12 15:58:05 +0100

    Hot fix

--- 0x14-mysql/create_mysql_user_with_replication_client_grant.sql
+++
@@ -1,5 +0,0 @@
--- Create user and password with replication client grant
-
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
