09b461e7b0390cd7381225f581efc533ab66b738
root <root@DESKTOP-D3IO7NG.localdomain>
2023-06-08 20:00:59 +0100

    Include headers to the request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     response = response.json().get('data').get('subscribers')
