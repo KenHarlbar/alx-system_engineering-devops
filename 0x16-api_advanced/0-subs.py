ee101a685e27350afdf8ba494cf8e05ebdcd1408
root <root@DESKTOP-D3IO7NG.localdomain>
2023-06-08 21:11:37 +0100

    Make the script adherent to the pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,15 @@
 import requests
 
+"""
+Function that queries Reddit's API for total
+number of subscribers of a subreddit
+"""
+
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'\
+            .format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; \
+            Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
